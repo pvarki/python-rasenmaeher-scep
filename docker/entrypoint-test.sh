c2cd390a45233f370682c7342f714ebd6cda8b9d
benjamsf <benjam@opendefence.fi>
2026-09-13 00:22:37 +0300

    fix: run the hooks through the init script in the test container
    prek needs a git repository and .git is not in the image, so calling it
    directly aborted the whole test run.

--- docker/entrypoint-test.sh
+++ docker/entrypoint-test.sh
@@ -7,8 +7,9 @@ if [ "$#" -eq 0 ]; then
   find tests  -type d -name '__pycache__' -print0 | xargs -0 rm -rf {}
   # Make sure the service itself is installed
   uv sync --frozen
-  # Make sure pre-commit checks were not missed because reasons
-  prek run --all-files
+  # Make sure pre-commit checks were not missed because reasons. Via the init script because
+  # .git is not in the image and prek needs a repository.
+  docker/pre_commit_init.sh
   # Then run the tests
   pytest --junitxml=pytest.xml tests/
   # If prek does not run these, enable them
