661d9db60def642a5c58aea6c02a5bf56d4a78db
benjamsf <benjam@opendefence.fi>
2026-09-14 13:04:10 +0300

    chore: bump version

--- tests/test_rmscep.py
+++ tests/test_rmscep.py
@@ -5,4 +5,4 @@ from rmscep import __version__
 
 def test_version() -> None:
     """Make sure version matches expected"""
-    assert __version__ == "1.0.0+260912"
+    assert __version__ == "1.0.1+260914"
